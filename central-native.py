c318b034aa97441113438d41dc2a3218f3c938e4
s542932 <84051396+s542932@users.noreply.github.com>
2021-05-19 22:36:19 -0500

    Update central-native.py

--- central-native.py
+++ central-native.py
@@ -1,6 +1,5 @@
 values = [47,95,88,73,88,84]
-\n
-print ('2. The SUM of values on page 110-111, question 4 =',sum(values))
+print ('\n2. The SUM of values on page 110-111, question 4 =',sum(values))
 mean = sum(values) / len(values)
 print ('3. The MEAN of values on page 110-111, question 4 =',mean)
 #print ('4. The MEDIAN of values on page 110-11, question 4 =, median(values))
