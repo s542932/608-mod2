6f375ed837683482e819d4bec0b5ecdaac0127dd
s542932 <84051396+s542932@users.noreply.github.com>
2021-05-19 22:58:00 -0500

    Update central-native.py

--- central-native.py
+++ central-native.py
@@ -3,6 +3,6 @@ print ('\n1. The COUNT of values on page 110-111, question 4 =',len(values))
 print ('2. The SUM of values on page 110-111, question 4 =',sum(values))
 mean = sum(values) / len(values)
 from decimal import Decimal
-print ('3. The MEAN of values on page 110-111, question 4 =', f'{Decimal(mean):.2f'})
+print ('3. The MEAN of values on page 110-111, question 4 =', f"{Decimal(mean):.2f"})
 #print ('4. The MEDIAN of values on page 110-11, question 4 =', median(values))
 #print ('5. The MODE of values on page 110-111, question 4 =', mode(values),'\n')
