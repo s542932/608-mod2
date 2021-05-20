7c0e85fb3a8809623027cac3f598a3afb988688e
s542932 <84051396+s542932@users.noreply.github.com>
2021-05-19 22:30:38 -0500

    Update central-native.py

--- central-native.py
+++ central-native.py
@@ -1,2 +1,6 @@
 values = [47,95,88,73.88,84]
-print ('2. Sum =',sum(values))
+print ('2. The SUM of values on page 110-111, question 4 =',sum(values))
+mean = sum(values) / len(values)
+print ('3. The MEAN of values on page 110-111, question 4 =',mean)
+print ('4. The MEDIAN of values on page 110-11, question 4 =, median(values))
+print ('5. The MODE of values on page 110-111, question 4 = mode(values))
