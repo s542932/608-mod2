1d55de0fa71ac6d5195f2ea94fcf6dc091935925
s542932 <84051396+s542932@users.noreply.github.com>
2021-05-21 21:22:07 -0500

    Update central-native.py

--- central-native.py
+++ central-native.py
@@ -1,9 +1,20 @@
 values = [47,95,88,73,88,84]
+
 print ('\n1. The COUNT of values on page 110-111, question 4 =',len(values))
+
 print ('2. The SUM of values on page 110-111, question 4 =',sum(values))
+
 mean = sum(values) / len(values)
 print (f'3. The MEAN of values on page 110-111, question 4 = {(mean):.2f}')
+
 sortedlist = sorted(values)
-print(sortedlist)
-#print ('4. The MEDIAN of values on page 110-11, question 4 =', median(values))
+n = len(values)
+if n % 2 = 0:
+  median1 = sortedlist[n//2]
+  median2 = sortedlist[n//2-1]
+  median = (median1 + median2)/2
+else:
+  median = sortedlist_num[n//2]
+print ('4. The MEDIAN of values on page 110-11, question 4 =', median)
+
 #print ('5. The MODE of values on page 110-111, question 4 =', mode(values),'\n')
