5baf3ae7c548c3ed709fd590032a1be2626eaf2e
s542932 <84051396+s542932@users.noreply.github.com>
2021-05-22 06:42:46 -0500

    Update central-native.py

--- central-native.py
+++ central-native.py
@@ -21,7 +21,7 @@ y = sortedlist
 L1 = []
 i = 0
 while i < len(y):
-  l1.append(y.count(y[i])
+  L1.append(y.count(y[i])
   i += 1
 d1 = dict(zip(y,L1))
 d2 = (k for (k,v) in d1.items() if v == max(L1))
