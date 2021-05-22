ba68d8997e79beac1b8849c728d665aa7ae8298d
s542932 <84051396+s542932@users.noreply.github.com>
2021-05-21 22:42:49 -0500

    Update central-with-statistics.py

--- central-with-statistics.py
+++ central-with-statistics.py
@@ -1,11 +1,11 @@
 values = [47,95,88,73,88,84]
 import statistics
 
-print ('\nPython Stastics to determine the mean, median, and mode in this example.\n\n1. The COUNT of values on page 110-111, question 4 =',len(values))
+print ('\nPython Stastics was used to determine the mean, median, and mode in this example.\n\n1. The COUNT of values on page 110-111, question 4 =',len(values))
 
 print ('2. The SUM of values on page 110-111, question 4 =',sum(values))
 
-print ('3. The MEAN of values on page 110-111, question 4 =',statistics.mean(values))
+print (f'3. The MEAN of values on page 110-111, question 4 = {statistics.mean(values):.2f}')
 
 print ('4. The MEDIAN of values on page 110-11, question 4 =',statistics.median(values))
 
